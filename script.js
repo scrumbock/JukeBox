76de059add2f97713ecdeb4de786377b9a0fe2c9
scrumbock <scrumboc@yahoo.com>
2017-04-24 18:09:04 -0400

    added stuff

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function Jukebox(song, artist) {
-	this.song = song;
-	this.artist = artist;
+function Jukebox() {
+	// this.artist = artist;
 	this.playSong = playsong;
 	this.pauseSong = pauseSong;
 	this.stopSong = stopSong;
@@ -9,9 +8,50 @@ function Jukebox(song, artist) {
 	this.switchSong = switchSong;
 }
 
-function Playlist(artistName) {
-	this.artistName = artistName;
-	this.galaList = [];
+var audio = document.getElementsByClassName("audio")[0];
+audio.src = "lush.mp3"
+// Do i need variables (instances) for the above functions?(methods?)
+
+
+
+var song =('<source src=')+(songTitle)+('>')
+
+
+// var playButton = document.getElementsByClassName("play")[0];
+// var lushLight = document.getElementsByClassName("light")[1];
+// playButton.addEventListener("click", lushLight.play);
+
+var suzannJukebox = new Jukebox();
+
+function Song(fileName, artist) {
+	this.fileName = fileName;
+	this.artist = artist;
 }
 
-var gala = new Playlist 
+var hypocrite = new Song("hypocrite.mp3", "Lush")
+var light = new Song("lush.mp3")
+
+playButton.addEventListener("click", playSong)
+function playSong() {
+	audio.play();
+}
+
+function Playlist(playlistName) {
+	this.playlistName = playlistName;
+	this.songList = [];
+	this.addSong = addSong;
+}
+
+
+function addSong(fileName) {
+	this.songList.push(Song);
+}
+
+
+addSong("lush.mp3")
+
+
+
+var lush = new Playlist("lush.mp3"); 
+
+
